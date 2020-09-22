40a18df23e664883b411ddf10021ea154876048f
Ignas Deltuva <ideltuva@ideltuvas-MacBook-Pro.local>
2020-09-22 10:36:48 +0300

    Setup production webpack build

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,12 @@
 const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 console.log(path.join(__dirname, 'public'));
 
 module.exports = (env) => {
     const isProduction = env === 'production';
-    
+    const CSSExtract = new ExtractTextPlugin('styles.css');
+
     return {
         entry: './src/app.js',
         output: {
@@ -18,13 +20,27 @@ module.exports = (env) => {
                 exclude: /node_modules/
             }, {
                 test: /\.s?css$/,
-                use:[
-                    'style-loader',
-                    'css-loader',
-                    'sass-loader'
-                ]
+                use: CSSExtract.extract({
+                    use: [
+                        {
+                            loader: 'css-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                })
             }]
         },
+        plugins: [
+            CSSExtract
+        ],
         devtool: isProduction? 'source-map' : 'cheap-module-eval-source-map',
         devServer: {
             contentBase: path.join(__dirname, 'public'),
